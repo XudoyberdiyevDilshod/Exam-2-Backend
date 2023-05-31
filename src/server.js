082b0e5ad6f54e0def88bc8a73c43e21f90994cf
XudoyberdiyevDilshod <1998xudoyberdiev@gmail.com>
2023-05-31 21:25:06 +0500

    add morgan

--- src/server.js
+++ src/server.js
@@ -1,7 +1,9 @@
 import express from "express";
 import fileUpload from "express-fileupload";
-import { resolve } from "path";
+import fs from "fs";
+import { resolve, join } from "path";
 import cors from "cors";
+import morgan from "morgan";
 import errorHandler from "./middleware/errorHandler.js";
 import adminRouter from "./router/admin.route.js";
 import postRouter from "./router/post.route.js";
@@ -14,6 +16,17 @@ app.use(cors());
 app.use(express.static(resolve("uploads")));
 app.use(express.json());
 app.use(fileUpload());
+
+app.use(
+  morgan("combined", {
+    skip: function (req, res) {
+      return res.statusCode < 400;
+    },
+    stream: fs.createWriteStream(join(process.cwd(), "access.log"), {
+      flags: "a",
+    }),
+  })
+);
 
 app.use(adminRouter);
 app.use(postRouter);
