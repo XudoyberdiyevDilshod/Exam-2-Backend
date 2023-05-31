e22b3ff2caad673eebee10dda806a9fc75bc5a65
XudoyberdiyevDilshod <1998xudoyberdiev@gmail.com>
2023-05-31 14:42:51 +0500

    create errorHandler

--- src/server.js
+++ src/server.js
@@ -2,6 +2,7 @@ import express from "express";
 import fileUpload from "express-fileupload";
 import { resolve } from "path";
 import cors from "cors";
+import error from "./middleware/errorHandler.js";
 
 const PORT = process.env.PORT || 7000;
 
@@ -13,5 +14,6 @@ app.use(express.json());
 app.use(fileUpload());
 
 
+app.use(error);
 
 app.listen(PORT, () => console.log("server running port: " + PORT));
